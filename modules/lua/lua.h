d2525be07d98d129d9f769d49b57bcc4d3343060
Tusa Viktor <tusavik@gmail.com>
2014-01-05 17:24:10 +0100

    lua: Added simple version of lua destination.
    Usage:
    
      destination d_lua {
        lua(
          script("test.lua")
          init-func("test_init")
          queue-func("test_queue")
          template("$MESSAGE")
        );
      };
    
    Parameters:
     * script: the name of the Lua file which contains the functions for the
       destination.
     * init-func: the name of the Lua function which is called during
       initialization of the destination.
     * queue-func: each message reaching this destination is formatted using
       the template parameter, and passed to this function as a Lua string.
     * template: can be omitted, it's default value is "$MESSAGE"
    
    Each lua destination has it's own state, so init and queue functions can
    have side effects, and global variables are kept between init and
    queue calls.
    
    Example lua file:
    
    --BEGIN
    function test_init()
      print "Init called!"
      counter = 0
    end
    
    function test_queue
      counter = counter + 1
      print(msg  .. " " .. tostring(counter))
    end
    --END

---
+++ modules/lua/lua.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2013, 2014 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2013, 2014 Viktor Tusa <tusa@balabit.hu>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef _AFLUA_DEST_H
+#define _AFLUA_DEST_H
+
+#include "driver.h"
+#include "logwriter.h"
+#include <lua.h>
+
+typedef struct _LuaDestDriver
+{
+  LogDestDriver super;
+  lua_State* state;
+  gchar* template_string;
+  gchar* filename;
+  gchar* init_func_name;
+  gchar* queue_func_name;
+  LogTemplate* template;
+  LogTemplateOptions template_options;
+} LuaDestDriver;
+
+LogDriver* lua_dd_new();
+void lua_dd_set_init_func(LogDriver* d, gchar* init_func_name);
+void lua_dd_set_queue_func(LogDriver* d, gchar* queue_func_name);
+void  lua_dd_set_filename(LogDriver* d, gchar* filename);
+void lua_dd_set_template(LogDriver* d, LogTemplate* template);
+LogTemplateOptions* lua_dd_get_template_options(LogDriver *d);
+
+#endif
