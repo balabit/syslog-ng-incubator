2d535fb5dfe865b1000d3930cd048af9eaad90f1
Gergely Nagy <algernon@madhouse-project.org>
2014-03-28 13:02:31 +0100

    getent: Split into multiple files
    For easier maintenance, split tfgetent.c into smaller files.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- modules/getent/tfgetent.c
+++ modules/getent/tfgetent.c
@@ -38,29 +38,12 @@
 typedef gboolean (*lookup_method)(gchar *key, gchar *member_name, GString *result);
 typedef gboolean (*format_member)(gchar *member_name, gpointer member, GString *result);
 
-static gboolean
-tf_getent_services(gchar *key, gchar *member_name, GString *result)
+typedef struct
 {
-  struct servent serv, *res;
-  glong d;
-  gboolean is_num;
-  char buf[4096];
-
-  if ((is_num = parse_number(key, &d)) == TRUE)
-    getservbyport_r((int)ntohs(d), NULL, &serv, buf, sizeof(buf), &res);
-  else
-    getservbyname_r(key, NULL, &serv, buf, sizeof(buf), &res);
-
-  if (res == NULL)
-    return TRUE;
-
-  if (is_num)
-    g_string_append(result, res->s_name);
-  else
-    g_string_append_printf(result, "%i", htons(res->s_port));
-
-  return TRUE;
-}
+  gchar *member_name;
+  format_member format;
+  size_t offset;
+} formatter_map_t;
 
 static gboolean
 _getent_format_string(gchar *member_name, gpointer member, GString *result)
@@ -90,23 +73,6 @@ _getent_format_uid_gid(gchar *member_name, gpointer member, GString *result)
   return TRUE;
 }
 
-typedef struct
-{
-  gchar *member_name;
-  format_member format;
-  size_t offset;
-} formatter_map_t;
-
-static formatter_map_t passwd_field_map[] = {
-  { "name", _getent_format_string, offsetof(struct passwd, pw_name) },
-  { "uid", _getent_format_uid_gid, offsetof(struct passwd, pw_uid) },
-  { "gid", _getent_format_uid_gid, offsetof(struct passwd, pw_gid) },
-  { "gecos", _getent_format_string, offsetof(struct passwd, pw_gecos) },
-  { "dir", _getent_format_string, offsetof(struct passwd, pw_dir) },
-  { "shell", _getent_format_string, offsetof(struct passwd, pw_shell) },
-  { NULL, NULL, 0 }
-};
-
 static int
 _find_formatter(formatter_map_t *map, gchar *member_name)
 {
@@ -122,70 +88,8 @@ _find_formatter(formatter_map_t *map, gchar *member_name)
   return -1;
 }
 
-static gboolean
-tf_getent_passwd(gchar *key, gchar *member_name, GString *result)
-{
-  struct passwd pwd;
-  struct passwd *res;
-  char *buf;
-  long bufsize;
-  int s;
-  glong d;
-  gboolean is_num, r;
-
-  bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
-  if (bufsize == -1)
-    bufsize = 16384;
-
-  buf = g_malloc(bufsize);
-
-  if ((is_num = parse_number(key, &d)) == TRUE)
-    s = getpwuid_r((uid_t)d, &pwd, buf, bufsize, &res);
-  else
-    s = getpwnam_r(key, &pwd, buf, bufsize, &res);
-
-  if (res == NULL && s != 0)
-    {
-      msg_error("$(getent passwd) failed",
-                evt_tag_str("key", key),
-                evt_tag_errno("errno", errno),
-                NULL);
-      g_free(buf);
-      return FALSE;
-    }
-
-  if (member_name == NULL)
-    {
-      if (is_num)
-        member_name = "name";
-      else
-        member_name = "uid";
-    }
-
-  if (res == NULL)
-    {
-      g_free(buf);
-      return FALSE;
-    }
-
-  s = _find_formatter(passwd_field_map, member_name);
-
-  if (s == -1)
-    {
-      msg_error("$(getent passwd): unknown member",
-                evt_tag_str("key", key),
-                evt_tag_str("member", member_name),
-                NULL);
-      g_free(buf);
-      return FALSE;
-    }
-
-  r = passwd_field_map[s].format(member_name,
-                                 ((uint8_t *)res) + passwd_field_map[s].offset,
-                                 result);
-  g_free(buf);
-  return r;
-}
+#include "getent-services.c"
+#include "getent-passwd.c"
 
 static struct
 {
