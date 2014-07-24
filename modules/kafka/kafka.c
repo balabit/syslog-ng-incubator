35a6f4341b7c6d5bdd184b5ee702c23fffcffcad
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-07-24 11:07:38 +0200

    wire kafka messages internally

--- modules/kafka/kafka.c
+++ modules/kafka/kafka.c
@@ -84,6 +84,13 @@ typedef struct
 } KafkaDriver;
 
 void
+kafka_log(rd_kafka_t *rkt, int level,
+          const char *fac, const char *msg)
+{
+    msg_send_formatted_message(level, msg);
+}
+
+void
 kafka_property_free(void *p)
 {
     struct kafka_property *kp = p;
@@ -130,6 +137,7 @@ kafka_dd_set_props(LogDriver *d, GList *props)
       rd_kafka_conf_set(conf, kp->key, kp->val,
                         errbuf, sizeof(errbuf));
   }
+  rd_kafka_conf_set_log_cb(conf, kafka_log);
   self->kafka = rd_kafka_new(RD_KAFKA_PRODUCER, conf,
                              errbuf, sizeof(errbuf));
 }
