ba0793f3f15d1b6108d4cf785eba7eaa8f9dc70d
Laszlo Meszaros <lmesz@balabit.hu>
2014-09-22 16:23:06 +0200

    ZMQ: removed unused zmq_install.sh
     * It doesn't belong to incubator. It needed for travis only.

--- modules/zmq/zmq_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/zeromq/libzmq/archive/master.zip -O /tmp/master.zip
-
-cd /tmp
-unzip master.zip
-mkdir $HOME/install/libzmq
-cd libzmq-master && ./autogen.sh && ./configure --prefix=$HOME/install/libzmq && make && make install
