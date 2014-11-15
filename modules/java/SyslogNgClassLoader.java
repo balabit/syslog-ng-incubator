5392df4dc69ad59703f7800a2659c2447d8d5cc2
Juhász Viktor <viktor.juhasz@balabit.com>
2014-11-15 15:26:39 +0100

    java-dest: the classloader should be a specific per destination,
    but it should use the same ClassLoader for loading all the classes,
    else the objects can't use each other.
    
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- modules/java/SyslogNgClassLoader.java
+++ modules/java/SyslogNgClassLoader.java
@@ -7,7 +7,8 @@ import java.net.URLClassLoader;
 
 
 public class SyslogNgClassLoader {
-  private static URL[] createUrls(String pathList) {
+  private ClassLoader classLoader;
+  private URL[] createUrls(String pathList) {
     String[] pathes = pathList.split(":");
     URL[] urls = new URL[pathes.length];
     int i = 0;
@@ -22,17 +23,24 @@ public class SyslogNgClassLoader {
     return urls;
   }
 
-  public static Class loadClassFromPathList(String pathList, String className) {
-    Class<?> result = null;
-    URL[] urls = createUrls(pathList); 
+  public SyslogNgClassLoader(String pathList) {
+    URL[] urls = createUrls(pathList);
+    classLoader = new URLClassLoader(urls, System.class.getClassLoader());
+  }
 
-    ClassLoader cl = new URLClassLoader(urls, System.class.getClassLoader());
+  public Class loadClass(String className) {
+    Class result = null;
     try {
-      result = Class.forName(className, true, cl);
+      result = Class.forName(className, true, classLoader);
     }
-    catch (Exception e) {
-      e.printStackTrace();
+    catch (ClassNotFoundException e) {
+      System.out.println("Exception: " + e.getMessage());
+      e.printStackTrace(System.err);
+    }
+    catch (NoClassDefFoundError e) {
+      System.out.println("Error: " + e.getMessage());
+      e.printStackTrace(System.err);
     }
     return result;
-  }  
+  }
 }
