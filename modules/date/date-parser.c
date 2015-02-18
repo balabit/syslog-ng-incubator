4764270be809b3677f5baba0a8ad5e785e257353
Vincent Bernat <vincent@bernat.im>
2015-02-18 17:35:30 +0100

    date-parser: allow a user to set timezone for timezone-less dates
    When the parsed date doesn't come with a timezone, allow a user to set
    another timezone using the `time-zone()` parameter. The timezone can be
    expressed as `[+-]HH:MM` or as a name (like `Europe/Paris`).
    
    Behavior of `mktime()` is quite undocumented. Tests are mandatory to
    detect any problem across supported platforms.
    
    Signed-off-by: Vincent Bernat <vincent@bernat.im>

--- modules/date/date-parser.c
+++ modules/date/date-parser.c
@@ -31,6 +31,8 @@ typedef struct _DateParser
   LogParser super;
   goffset date_offset;
   gchar *date_format;
+  gchar *date_tz;
+  TimeZoneInfo *date_tz_info;
 } DateParser;
 
 void
@@ -47,6 +49,18 @@ void date_parser_set_format (LogParser *s, gchar *format)
     g_free (self->date_format);
 
   self->date_format = g_strdup (format);
+}
+
+void date_parser_set_timezone (LogParser *s, gchar *tz)
+{
+  DateParser *self = (DateParser *)s;
+  if (self->date_tz)
+    g_free (self->date_tz);
+
+  self->date_tz = g_strdup (tz);
+  if (self->date_tz_info)
+    time_zone_info_free (self->date_tz_info);
+  self->date_tz_info = time_zone_info_new (self->date_tz);
 }
 
 static gboolean
@@ -87,9 +101,25 @@ date_parser_process (LogParser *s,
 
   /* mktime handles timezones horribly. It considers the time to be
      local and also alter the parsed timezone. Try to fix all that. */
-  msg->timestamps[LM_TS_STAMP].zone_offset = tm.tm_gmtoff;
-  msg->timestamps[LM_TS_STAMP].tv_sec = mktime(&tm) - msg->timestamps[LM_TS_STAMP].zone_offset - timezone;
   msg->timestamps[LM_TS_STAMP].tv_usec = 0;
+  if (!self->date_tz_info)
+    {
+      msg->timestamps[LM_TS_STAMP].zone_offset = tm.tm_gmtoff;
+      msg->timestamps[LM_TS_STAMP].tv_sec = mktime (&tm) - msg->timestamps[LM_TS_STAMP].zone_offset - timezone;
+    }
+  else
+    {
+      msg->timestamps[LM_TS_STAMP].tv_sec = mktime (&tm) - timezone;
+      msg->timestamps[LM_TS_STAMP].zone_offset =
+        time_zone_info_get_offset(self->date_tz_info,
+                                  msg->timestamps[LM_TS_STAMP].tv_sec);
+      if (msg->timestamps[LM_TS_STAMP].zone_offset == -1)
+        {
+          msg->timestamps[LM_TS_STAMP].zone_offset =
+            get_local_timezone_ofs(msg->timestamps[LM_TS_STAMP].tv_sec);
+        }
+      msg->timestamps[LM_TS_STAMP].tv_sec -= msg->timestamps[LM_TS_STAMP].zone_offset;
+    }
 
   return TRUE;
 };
@@ -100,9 +130,14 @@ date_parser_clone (LogPipe *s)
   DateParser *self = (DateParser *) s;
 
   DateParser *cloned = (DateParser *) date_parser_new (log_pipe_get_config (&self->super.super));
-  g_free(cloned->date_format);
+  g_free (cloned->date_format);
+  g_free (self->date_tz);
+  if (self->date_tz_info)
+    time_zone_info_free (self->date_tz_info);
   cloned->date_offset = self->date_offset;
   cloned->date_format = g_strdup (self->date_format);
+  cloned->date_tz = g_strdup (self->date_tz);
+  cloned->date_tz_info = time_zone_info_new (cloned->date_tz);
   cloned->super.template = log_template_ref (self->super.template);
 
   return &cloned->super.super;
@@ -114,6 +149,9 @@ date_parser_free (LogPipe *s)
   DateParser *self = (DateParser *)s;
 
   g_free (self->date_format);
+  g_free (self->date_tz);
+  if (self->date_tz_info)
+    time_zone_info_free (self->date_tz_info);
 
   log_parser_free_method (s);
 };
