6f2dd3e2c73547e0f1362e0cf08995389594d35f
Laszlo Meszaros <lacienator@gmail.com>
2015-12-01 21:05:03 +0000

    zmq(wip): one little step toward zmq-func-test

--- modules/zmq/tests/functional/test_zmq_source.py
+++ modules/zmq/tests/functional/test_zmq_source.py
@@ -4,4 +4,30 @@ import os
 import sys
 
 sys.path.append(os.environ['func_test_dir'])
+
 from control import *
+from func_test import *
+
+conf = """
+@version: 3.7
+
+source zmq {
+    zmqq();
+};
+
+destination file {
+    file("/tmp/res");
+};
+
+log {
+    source(zmq);
+    destination(file);
+};
+"""
+
+def test_zmq_source_can_receive_a_message():
+    if not start_syslogng(conf, keep_persist=False, verbose=False):
+        sys.exit(42)
+    stop_syslogng()
+
+test_zmq_source_can_receive_a_message()
