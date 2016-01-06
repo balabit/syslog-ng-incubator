c6cc7316acd3533bce22f4b480da7fa642007be4
Laszlo Meszaros <lacienator@gmail.com>
2016-01-06 18:40:54 +0100

    zmq: waaa func-test

--- modules/zmq/tests/functional/test_zmq_source.py
+++ modules/zmq/tests/functional/test_zmq_source.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
-sys.path.append(os.environ['func_test_dir'])
-
 from control import *
-from func_test import *
 
 conf = """
 @version: 3.8
@@ -29,5 +23,3 @@ def test_zmq_source_can_receive_a_message():
     if not start_syslogng(conf, keep_persist=False, verbose=False):
         sys.exit(42)
     stop_syslogng()
-
-test_zmq_source_can_receive_a_message()
